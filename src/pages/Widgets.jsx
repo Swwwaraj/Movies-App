2649da18317d742d408aa5fc72bbd46642af11f2
Swwwaraj <zavareswaraj226@gmail.com>
2024-12-12 20:25:27 +0530

    feat: inital commit

---
+++ src/pages/Widgets.jsx
@@ -0,0 +1,22 @@
+import Browser from "../Component/Browser";
+import News from "../Component/News";
+import Note from "../Component/Note";
+import Timer from "../Component/Timer";
+import UserInfo from "../Component/UserInfo";
+import Weather from "../Component/Weather";
+import './Widgets.css'
+export default function Widgets() {
+    return (
+        <div className="wid-group">
+            <div className="wid-style">
+                <UserInfo />
+                <Note />
+                {/* <News/> */}
+                {/* <Weather/> */}
+                <Timer duration={24 * 60 * 60 * 1000} />
+                <Browser />
+            </div>
+
+        </div>
+    );
+}
